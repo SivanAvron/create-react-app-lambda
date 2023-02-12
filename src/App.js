481c3c5d4593d4dc545f5653c7efe1d54a11c849
SivanAvron <47516801+SivanAvron@users.noreply.github.com>
2023-02-12 11:33:22 +0200

    sivana dot com

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            sivana dot com is mine.
           </p>
           <LambdaDemo />
         </header>
